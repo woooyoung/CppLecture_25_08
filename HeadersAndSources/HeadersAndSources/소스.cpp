a5d016855bee6d2cf23d43b090522545b4f8a32e
woooyoung <axdsw121@gmail.com>
2025-08-04 14:33:28 +0900

    헤더파일과 소스파일

---
+++ HeadersAndSources/HeadersAndSources/소스.cpp
@@ -0,0 +1,13 @@
+#include "Çì´õ.h"
+
+int main() {
+	
+	printf("%f\n", pow((2.0), 10));
+
+	printf("%f\n", sin(1));
+
+	srand(time(0));
+	printf("%d\n", rand() % 100);
+
+	return 0;
+}
---
+++ HeadersAndSources/HeadersAndSources/헤더.h
@@ -0,0 +1,5 @@
+
+#include <cstdio>
+#include <cmath> // sin(), cos(), tan(), exp(), pow(), round(), ceil(), floor()
+#include <cstdlib> // malloc, free, rand, srand
+#include <ctime> // time, clock -> 의사 난수 시드 설정
