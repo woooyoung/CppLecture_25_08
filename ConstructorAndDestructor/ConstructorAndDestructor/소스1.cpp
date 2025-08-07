1632fcb419772773c4cfd7a2f76c7884d29b771d
woooyoung <axdsw121@gmail.com>
2025-08-07 14:37:46 +0900

    생성자와 소멸자

--- ConstructorAndDestructor/ConstructorAndDestructor/소스1.cpp
+++ ConstructorAndDestructor/ConstructorAndDestructor/소스1.cpp
@@ -8,18 +8,27 @@
 class Student {
 public: 
 	int student_no;
-	char name[100];
+	char* name;
 	int gender; // 0-남, 1-여
 
 	// 생성자(Constructor) 
 	// 생성자는 클래스 이름과 동일하게 만들어준다. 리턴타입은 x
 	// 생성자는 new를 통해 객체를 만들 때 그 시점에 자동실행된다.
+	// 객체가 생성되자마자 파라미터를 이용해서 값을 초기화 해준다.
 
 	Student(int StdNo, const char* n) { 
 		printf("생성자 실행됨\n");
 		this->student_no = StdNo;
+		name = new char[strlen(n) + 1];
 		strcpy(this->name, n);
 		this->gender = 0;
+
+		//delete[] name; --> 문제발생 : 정작 필요할 때 사용하지 못함
+	}
+
+	~Student() { // 소멸자 : 클래스 인스턴스가 파괴될 때 -> 메모리에서 해제될 때
+		printf("%s의 소멸자 호출됨\n", name);
+		delete[] name;
 	}
 
 	void printStudentInfo() { // -> 수동실행
@@ -42,6 +51,9 @@ int main() {
 	Student* ps2 = new Student(1234, "김철수");
 	ps2->printStudentInfo();
 	delete ps2;
+
+	Student ps3 = Student(4567, "박민수");
+	ps3.printStudentInfo();
 
 	return 0;
 }
---
+++ ConstructorAndDestructor/ConstructorAndDestructor/소스2.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdio>
+#include <cstdlib>
+
+int main() {
+
+	int arr[20];
+
+	int* arr2 = (int*)malloc(sizeof(int) * 20);
+
+	//free(arr2);
+
+	int* arr3 = new int[20];
+
+	//delete arr3;
+
+	return 0;
+}
