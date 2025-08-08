7af0d63dbc8405a0ace1b99bc090f9119ca1e76f
woooyoung <axdsw121@gmail.com>
2025-08-08 16:17:00 +0900

    상속

---
+++ InheritanceAndAccessModifier/InheritanceAndAccessModifier/소스.cpp
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+// 상속
+// 상속해주는 클래스의 변수, 함수를 외부에서도 활용할 수 있다.
+class Animal {
+public:
+	int legs;
+	char name[50];
+
+	Animal(int legs, const char* name) {
+		printf("동물 생성자 실행\n");
+		this->legs = legs;
+		strcpy(this->name,name);
+	}
+
+	void printInfo() {
+		printf("다리의 갯수 : %d\n", legs);
+		printf("이름 : %s\n", name);
+	}
+};
+
+class Dog : public Animal {
+public:
+	Dog() : Animal(4, "개") {
+		printf("개 생성자 실행\n");
+	}
+};
+
+// 속성과 기능은 자식클래스가 부모클래스보다 많거나 같다.
+class Person : public Animal{
+public:
+	char regist_no[30];
+
+	Person(const char* regist_no) : Animal(2,"사람"){
+		printf("사람 생성자 실행\n");
+		strcpy(this->regist_no, regist_no);
+		printf("regist_no : %s\n", regist_no);
+	}
+};
+
+int main() {
+
+	Animal* a;
+
+	Person* p = new Person("1234-5678");
+	p->printInfo();
+
+	Dog* d = new Dog();
+	d->printInfo();
+
+	//a = p; o
+	//a = d; o
+
+	//p = a; x
+	//d = a; x
+	//p = (Person*)a; // 형변환, casting
+
+	return 0;
+}
