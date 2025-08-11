f78f7197a679eee3563084baef0d919934ae0395
woooyoung <axdsw121@gmail.com>
2025-08-11 14:27:47 +0900

    상속과 접근지정자

--- InheritanceAndAccessModifier/InheritanceAndAccessModifier/소스.cpp
+++ InheritanceAndAccessModifier/InheritanceAndAccessModifier/소스.cpp
@@ -3,13 +3,16 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-// 상속
+// 상속, 접근지정자
 // 상속해주는 클래스의 변수, 함수를 외부에서도 활용할 수 있다.
+// 접근지정자 : public, protected, private
 class Animal {
-public:
+//public: // 누구나 접근 가능
+//protected: // 외부에서 접근 불가, 대신 내부에서 접근 가능, 부모자식 관계라면 접근가능
+private: // 해당 클래스 내부 외에는 접근 불가능, 상속 관계에서도 접근 불가
 	int legs;
 	char name[50];
-
+public:
 	Animal(int legs, const char* name) {
 		printf("동물 생성자 실행\n");
 		this->legs = legs;
@@ -39,6 +42,10 @@ public:
 		strcpy(this->regist_no, regist_no);
 		printf("regist_no : %s\n", regist_no);
 	}
+
+	void printLegs() {
+		printf("Person의 Legs : %d\n", this->legs);
+	}
 };
 
 int main() {
@@ -46,17 +53,13 @@ int main() {
 	Animal* a;
 
 	Person* p = new Person("1234-5678");
-	p->printInfo();
+	//p->printInfo();
+
+	printf("p->legs : %d\n", p->legs);
 
 	Dog* d = new Dog();
-	d->printInfo();
+	//d->printInfo();
 
-	//a = p; o
-	//a = d; o
-
-	//p = a; x
-	//d = a; x
-	//p = (Person*)a; // 형변환, casting
 
 	return 0;
 }
