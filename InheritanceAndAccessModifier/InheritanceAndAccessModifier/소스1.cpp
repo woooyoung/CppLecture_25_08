7ab4797eefcbf3a9013aea3643a7e0c3e8e5267f
woooyoung <axdsw121@gmail.com>
2025-08-11 15:18:04 +0900

    상속 연습문제

---
+++ InheritanceAndAccessModifier/InheritanceAndAccessModifier/소스1.cpp
@@ -0,0 +1,36 @@
+﻿#include "헤더1.h"
+
+int main() {
+
+	Unit* units[10];
+
+	srand(time(0));
+
+	for (int i = 0; i < 10; i++) {
+		switch (rand() % 4) {
+		case 0:
+			units[i] = new Goblin();
+			//units[i]->max_hp = 100; xxxx 이렇게 하고싶지 않아서 생성자를 활용했다
+			break;
+		case 1:
+			units[i] = new Orc();
+			break;
+		case 2:
+			units[i] = new Slime();
+			break;
+		case 3:
+			units[i] = new SkeletonArcher();
+			break;
+		}
+	}
+
+	for (int i = 0; i < 10; i++) {
+		units[i]->printInfo();
+	}
+
+	for (int i = 0; i < 10; i++) {
+		delete units[i];
+	}
+
+	return 0;
+}
---
+++ InheritanceAndAccessModifier/InheritanceAndAccessModifier/헤더.h
@@ -0,0 +1,88 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+//판타지 게임의 적 캐릭터 설계를 하려고 한다.
+//
+//고블린의 최대 체력은 60이며 공격력을 5이고 사정거리는 10이며 이동속도는 10이다.
+//오크의 최대 체력은 80이며 공격력은 10이고 사정거리는 12이며 이동속도는 8이다.
+//슬라임의 최대 체력은 100이며 공격력은 3이고 사정거리는 5이며 이동속도는 4이다.
+//해골궁수의 최대 체력은 60이며 공격력은 20이고 사정거리는 50이며 이동속도는 3이다.
+//
+//이를 상속을 이용해 클래스 설계를 하여 코드를 작성해보자.
+//
+//모든 캐릭터는 자신의 이름이라는 속성이 존재하고
+//
+//printInfo라는 멤버 함수가 존재하며,
+//이 printInfo 라는 멤버 함수를 이용해 생성결과를 출력해보자.
+
+class Unit { // 부모클래스의 생성자를 활용하지 않았다.
+public:
+	int max_hp;
+	int atk_point;
+	int range;
+	int movement_speed;
+
+	char name[50];
+
+	void printInfo() {
+		printf("==%s의 속성==\n", name);
+		printf("최대 체력 : %d\n", max_hp);
+		printf("공격력 : %d\n", atk_point);
+		printf("사정거리 : %d\n", range);
+		printf("이동속도 : %d\n", movement_speed);
+		printf("\n");
+	}
+};
+
+class Goblin : public Unit {
+public:
+	Goblin() {
+		max_hp = 60;
+		atk_point = 5;
+		range = 10;
+		movement_speed = 10;
+
+		strcpy(name, "고블린");
+	}
+};
+
+class Orc : public Unit {
+public:
+	Orc() {
+		max_hp = 80;
+		atk_point = 10;
+		range = 12;
+		movement_speed = 8;
+
+		strcpy(name, "오크");
+	}
+};
+
+class Slime : public Unit {
+public:
+	Slime() {
+		max_hp = 100;
+		atk_point = 3;
+		range = 5;
+		movement_speed = 4;
+
+		strcpy(name, "슬라임");
+	}
+};
+
+class SkeletonArcher : public Unit {
+public:
+	SkeletonArcher() {
+		max_hp = 60;
+		atk_point = 20;
+		range = 50;
+		movement_speed = 3;
+
+		strcpy(name, "해골궁수");
+	}
+};
