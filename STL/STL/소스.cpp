d8281308d41ba27db64181d40cbcf2e95b0c7980
woooyoung <axdsw121@gmail.com>
2025-08-14 15:49:01 +0900

    STL, 가변배열

---
+++ STL/STL/소스.cpp
@@ -0,0 +1,49 @@
+﻿// STL(Standard Template Library)
+// 결정을 최대한 뒤로 미루고 싶어, 어떤 함수나 변수를 만들 때 타입? 몰라. 지정하고 싶지 않아.
+// 타입을 내가 미리 지정해놨을 때 ) int a = 10; int b(){return 5.5;}
+// -> 해당 타입의 특정적인 함수랑 변수 클래스 밖에 못 만든다.
+// STL -> 미루고 미루다가 타입을 사용하기 직전에 지정할래.(범용성이 높아진다)
+#include <cstdio>
+#include <string>
+
+template <typename T>
+class MyClass { // 아래의 클래스 내부에는 타입이 지정되지 않은 무언가가 있다. -> 타입의 지정을 뒤로 미룰래(선택장애)
+public:
+	T value; // <- 변수의 타입은 아직 정해지지 않았어.
+};
+
+template <typename T>
+void swap(T* a, T* b) {
+	T temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main() {
+
+	int v1 = 100;
+	int v2 = 200;
+
+	swap<int>(&v1, &v2);
+
+	printf("v1 : %d, v2 : %d\n", v1, v2);
+
+	std::string sv1 = "Hi";
+	std::string sv2 = "Bye";
+
+	swap<std::string>(&sv1, &sv2);
+
+	printf("sv1 : %s, sv2 : %s\n", sv1.c_str(), sv2.c_str());
+
+
+	MyClass<int>* m = new MyClass<int>(); // value 변수의 타입은 객체를 만들 때 정해졌다.
+	m->value = 100;
+	printf("%d\n", m->value);
+
+	MyClass<std::string>* ms = new MyClass<std::string>(); // value 변수의 타입은 객체를 만들 때 정해졌다.
+	ms->value = "안녕하세요";
+	printf("%s\n", ms->value.c_str());
+
+	return 0;
+}
---
+++ STL/STL/소스1.cpp
@@ -0,0 +1,91 @@
+﻿// 가변배열 -> 변하는게 가능한 배열
+// 고정크기 배열 -> 동적, 정적 크기가 정해진 배열
+
+#include <cstdio>
+#include <string>
+
+template <typename T>
+class MyArray {
+public:
+	T* arrayItem; // 배열 포인터
+	int count = 0; // 배열 요소 지금 몇개?
+	int capacity = 8; // 배열이 최대 몇개까지 요소를 넣을 수 있는가?
+
+	MyArray() {
+		arrayItem = new T[capacity];
+	}
+
+	virtual ~MyArray() {
+		delete[] arrayItem;
+	}
+
+	void putValue(T value) {
+		if (capacity > count) {
+			arrayItem[count] = value;
+			count++;
+		}
+		else {
+			printf("기존보다 2배 커진 배열 생성\n");
+			T* newArray = new T[capacity * 2];
+			capacity = capacity * 2;
+			for (int i = 0; i < count; i++) {
+				newArray[i] = arrayItem[i];
+			}
+			delete[] arrayItem; // 기존의 배열 메모리 해제
+			arrayItem = newArray; // 새로운 배열의 첫번째 공간의 주소로 덮어쓰기
+			arrayItem[count] = value;
+			count++;
+		}
+	}
+};
+
+int main() {
+
+	MyArray<int> m = MyArray<int>();
+	m.putValue(100);
+	m.putValue(200);
+	m.putValue(300);
+	m.putValue(400);
+	m.putValue(500);
+	m.putValue(600);
+	m.putValue(700);
+	m.putValue(800);
+
+	m.putValue(900);
+	m.putValue(1000);
+	m.putValue(1100);
+	m.putValue(1200);
+	m.putValue(1300);
+	m.putValue(1400);
+	m.putValue(1500);
+	m.putValue(1600);
+
+	m.putValue(1700);
+
+	for (int i = 0; i < m.count; i++) {
+		printf("%d ", m.arrayItem[i]);
+	}
+
+	printf("\n");
+
+	MyArray<std::string> ms = MyArray<std::string>();
+	ms.putValue("a");
+	ms.putValue("b");
+	ms.putValue("c");
+	ms.putValue("d");
+	ms.putValue("e");
+	ms.putValue("f");
+	ms.putValue("g");
+	ms.putValue("h");
+
+	ms.putValue("i");
+
+
+	for (int i = 0; i < ms.count; i++) {
+		printf("%s ", ms.arrayItem[i].c_str());
+	}
+
+	return 0;
+}
+
+
